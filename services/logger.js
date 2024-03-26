687664c6addaf1fd59b8e6af3f9ad42292e2b744
nandimandalam varneeth varma <86708658+varneeth01@users.noreply.github.com>
2024-03-26 23:55:53 +0530

    Update logger.js

--- services/logger.js
+++ services/logger.js
@@ -1,1 +1,23 @@
+const winston = require('winston');
+require('winston-mongodb');
 
+const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({filename:'app.log'}),
+    new winston.transports.MongoDB({
+      db: process.env.DATABASE_URI,
+      options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+      collection: 'logs', // Collection name in MongoDB
+      level: 'silly' // Log level
+    })
+],
+});
+
+
+module.exports = logger;
