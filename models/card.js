d83126dfe66f4f8503f5236f6448b4e0b88a3515
nandimandalam varneeth varma <86708658+varneeth01@users.noreply.github.com>
2024-03-30 19:15:23 +0530

    Update card.js

--- models/card.js
+++ models/card.js
@@ -1,1 +1,26 @@
+const mongoose = require("mongoose");
+const cardSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ["freemium", "premium", "bill", "event", "refer"],
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+  }, // for event cards
+});
 
+module.exports = mongoose.model("card", cardSchema);
