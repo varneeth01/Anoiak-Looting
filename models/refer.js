0857718433ef1c037f29caf8b032e107d5803467
nandimandalam varneeth varma <86708658+varneeth01@users.noreply.github.com>
2024-03-30 19:15:44 +0530

    Update refer.js

--- models/refer.js
+++ models/refer.js
@@ -1,10 +1,28 @@
-const mongoose = require('mongoose');
-const referralSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    referredUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+const mongoose = require("mongoose");
+const referralSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    referredUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     reward: {
-      type: { type: String, enum: ['pinkCash', 'greenCash', 'goldCoin', 'silverCoin'], required: true },
+      type: {
+        type: String,
+        enum: ["pinkCash", "greenCash", "goldCoin", "silverCoin"],
+        required: true,
+      },
       amount: { type: Number, required: true },
     },
-    createdAt: { type: Date, default: Date.now },
-  });
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Refer", referralSchema);
