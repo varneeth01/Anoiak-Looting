428a2f2f4d5a72399bd01c326d3d452234172363
nandimandalam varneeth varma <86708658+varneeth01@users.noreply.github.com>
2025-01-24 15:38:21 +0530

    Update verifyProvider.js

--- services/verifyProvider.js
+++ services/verifyProvider.js
@@ -6,13 +6,13 @@ const verifyFacebookToken = async (accessToken) => {
     const data = await response.json();
 
     if (data.data && data.data.is_valid && data.data.user_id) {
-      return true; // Token is valid
+      return true; 
     } else {
-      return false; // Token is invalid
+      return false; 
     }
   } catch (error) {
     console.error('Error verifying Facebook token:', error);
-    return false; // Error occurred while verifying token
+    return false; 
   }
 };
 
@@ -23,20 +23,15 @@ const verifyGoogleToken = async (idToken) => {
     const data = await response.json();
 
     if (data.aud === 'your_client_id') {
-      return true; // Token is valid
+      return true; 
     } else {
-      return false; // Token is invalid
+      return false; 
     }
   } catch (error) {
     console.error('Error verifying Google token:', error);
-    return false; // Error occurred while verifying token
+    return false;
   }
 };
-
-// // Usage
-// const isValid = await verifyGoogleToken('your_google_id_token');
-// const isValid = await verifyFacebookToken('your_facebook_access_token');
-
 
 module.exports ={
   verifyFacebookToken,
